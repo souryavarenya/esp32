7a0d62df4b2a11b53b7e3d3168830077f4550e35
souryavarenya <souryavarenya@gmail.com>
2019-01-06 23:55:10 +0530

    Tested Periodic Timer - Success

--- esp32_projects/periodic-timer/main/esp_timer_example_main.c
+++ esp32_projects/periodic-timer/main/esp_timer_example_main.c
@@ -1,12 +1,3 @@
-/* esp_timer (high resolution timer) example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
@@ -27,8 +18,6 @@ void app_main()
 {
     /* Create two timers:
      * 1. a periodic timer which will run every 0.5s, and print a message
-     * 2. a one-shot timer which will fire after 5s, and re-start periodic
-     *    timer with period of 1s.
      */
 
     const esp_timer_create_args_t periodic_timer_args = {
